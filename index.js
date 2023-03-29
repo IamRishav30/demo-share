93bd8590001c4abce278f02447ca78e19a208991
Subhradipta Choudhury <csubhradipta@gmail.com>
2023-03-29 20:28:15 +0530

    changes to io

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://demo-share.onrender.com',
-    methods: ['GET', 'POST']
+    origin: '*',
+    // methods: ['GET', 'POST']
   }
 });
 
