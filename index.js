a9bc6bfda39c9095db1e5a65852104c75ac58bbf
Subhradipta Choudhury <csubhradipta@gmail.com>
2023-03-29 20:17:38 +0530

    modified io

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://demo-textshare.netlify.app',
+    origin: 'https://demo-share.onrender.com',
     methods: ['GET', 'POST']
   }
 });
